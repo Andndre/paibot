2b2c90cc42352588222ad307d1d1df11f7a85ae9
AgungAndre01 <agungandre687@gmail.com>
2021-12-21 21:14:36 +0800

    commit heroku

--- main.py
+++ main.py
@@ -3,26 +3,14 @@ import os
 from discord import Intents
 from discord import Embed, activity
 from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_option
 from dotenv import load_dotenv
 
 import config.botinfo as inf
 
 load_dotenv()
 bot = commands.Bot(inf.prefix, activity = activity.Game(name=f'Type {inf.prefix}help for usage!'), help_command = None, intents=Intents.all())
-slash = SlashCommand(bot, sync_commands=True)
 
 guild_ids = [919177496669351956]
-
-@slash.slash(name="add", guild_ids=guild_ids, options=[
-  create_option(name='first number', required=True),
-  create_option(name='second number', required=True)
-  ]
-)
-async def test(ctx: SlashContext):
-  embed = Embed(title="Test")
-  await ctx.send(embed=embed)
 
 @bot.event
 async def on_ready():
