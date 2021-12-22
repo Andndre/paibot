f0210d8eb738aba6e3b571eeaa122397943fb789
AgungAndre01 <agungandre687@gmail.com>
2021-12-23 06:05:17 +0800

    fix dotenv error on heroku

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from discord import Intents
 from discord import activity
 from discord.ext import commands
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 import config.botinfo as inf
 
-load_dotenv()
+# load_dotenv()
 
 bot = commands.Bot(inf.prefix, activity = activity.Game(name=f'Type {inf.prefix}help for usage!'), help_command = None, intents=Intents.all())
 
@@ -20,4 +20,4 @@ bot.load_extension('cogs.help_command')
 bot.load_extension('cogs.math_commands')
 bot.load_extension('cogs.other_commands')
 
-bot.run(os.getenv('TOKEN'))
+bot.run(os.environ.get('TOKEN'))
