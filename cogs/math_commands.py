e594670b9ffd4ad9e25556b2da76a7c5263b60b2
AgungAndre01 <agungandre687@gmail.com>
2021-12-23 14:32:43 +0800

    update `calc` command
    - calc: when users use `calc` command, they can edit their message, then the bot will update the result
    - calc: now the bot will ignore unnecessary brackets instead of throwing an error

--- cogs/math_commands.py
+++ cogs/math_commands.py
@@ -2,6 +2,8 @@ import asyncio
 import re
 import traceback
 
+from discord import message
+
 from config import embeds_
 from discord.colour import Colour
 from discord.embeds import Embed
@@ -11,6 +13,15 @@ from discord.ext.commands.context import Context
 from discord.message import Message
 from utils.calc import calc_
 from utils.mquiz import generate_quiz
+
+def get_calc_result(mth):
+  result, err = calc_(mth)
+  if err:
+    return embeds_.error_embed(result)
+  em = Embed()
+  em.add_field(name='Result:',value=result, inline=False)
+  em.colour = Colour.blurple()
+  return em
 
 class MathCommands(commands.Cog):
   def __init__(self, bot):
@@ -27,10 +38,19 @@ class MathCommands(commands.Cog):
       em = Embed()
       em.add_field(name='Result:',value=result, inline=False)
       em.colour = Colour.blurple()
-      await ctx.reply(embed=em)
+      msg : Message = await ctx.reply(embed=em)
+
+      def check(before, after):
+        return after == ctx.message
+      while True:
+        before, after = await self.bot.wait_for('message_edit', check=check, timeout=200)
+        await msg.edit(embed=get_calc_result(ctx.message.content.replace('_calculator', '').replace('_calc', '')))
+    except asyncio.TimeoutError:
+      pass
     except Exception as e:
+      await msg.delete()
       traceback.print_exc()
-      await ctx.reply(embed=embeds_.error_embed(f'Something is wrong with your input, please try to enter the operator and parentheses correctly\n\n"{str(e)}"'))
+      await ctx.reply(embed=embeds_.error_embed(f'Something is wrong with your input\ntry: remove unnecessary brackets\n\n"{str(e)}"\n\nevent stopped'))
       return
   
   @commands.command(aliases=['math_quiz'])
--- utils/calc.py
+++ utils/calc.py
@@ -1,23 +1,9 @@
 import re
 from typing import Any, Tuple
 
-def findnth(string : str, target : str, nth : int) -> str:
-    parts= string.split(target, nth+1)
-    if len(parts)<=nth+1:
-      return -1
-    return len(string)-len(parts[-1])-len(target)
-
 def calc_(mth : str) -> Tuple[Any, bool]:
-  if re.search(r'[^0-9+\-*/:()Xx^\s]', mth):
-    return 'Accepts only numbers and symbols `+ - * x : / ( ) ^`', True
-  op = mth.count('(')
-  cl = mth.count(')')
-  if op != cl:
-    opcl = "opening" if op > cl else "closing"
-    return f'There are too many {opcl} brackets somewhere', True
-  return str(eval(validate(mth))), False
-
-def validate(mth : str) -> str:
+  if re.search(r'[^0-9+\-*/:()Xx^.,\s]', mth):
+    return 'Accepts only numbers and symbols `+ - * x : / ( ) ^ . ,`', True
   mth = (
     mth
     .replace('++', '+')
@@ -28,29 +14,75 @@ def validate(mth : str) -> str:
     .replace('x', '*')
     .replace('X', '*')
     .replace(':', '/')
+    .replace(',', '.')
+    .replace(' ', '')
   )
+  op = mth.count('(')
+  cl = mth.count(')')
+  if op != cl:
+    opcl = "opening" if op > cl else "closing"
+    return f'There are too many {opcl} brackets somewhere', True
+  step1 = validate(mth)
+  # print('step 1:',step1)
+  step2 = calc_2(step1.replace(' ', ''))
+  # print('step 2:',step2)
+  return step2, False
+
+def validate(mth : str) -> str:
 
   # add missing * before ( or after )
-  j = 0
-  operators = ['+','-','*','/']
+  j = 1
+  operators = ['+','-','*','/', '^']
   while(True):
-    # Here we check for the '(' and ')' together because 
-    # the number of occurrences is the same,
-    # unless the input is invalid
-    i = findnth(mth, '(', j)
-    if i-1 in [-1, -2]: break
-    # founded '(', checks if the previous 
-    # character is not an 'operator' and '('
-    if mth[i-1] not in operators and mth[i-1] != '(':
-      # insert '*' at i
-      mth = mth[:i] + '*' + mth[i:]
-    i = findnth(mth, ')', j)
-    # check if i+1 is the end of mth string
-    if i+1 == len(mth): break
-    # founded ')', checks if the next 
-    # character is not an 'operator' and ')'
-    if mth[i+1] not in operators and mth[i+1] != ')':
-      # insert '*' at i+1
-      mth = mth[:i+1] + '*' + mth[i+1:]
+    if j == len(mth)-1:
+      break
+    if mth[j] == '(':
+      if mth[j-1] not in operators and mth[j-1] != '(':
+        # insert '*' at i
+        mth = mth[:j] + '*' + mth[j:]
+    elif mth[j] == ')':
+      if mth[j+1] not in operators and mth[j+1] != ')':
+        # insert '*' at i+1
+        mth = mth[:j+1] + '*' + mth[j+1:]
     j+=1
   return mth
+
+def calc_2(mth : str):
+
+  # Calculate
+
+  # this variable will store all mth inside a brackets, 
+  # then computed by this function again (if it has bracket pair)
+  # recursively until the braces is gone
+  tmp = ''
+  final = ''
+  unclosed = 0
+  got_brace = False
+  for n in mth:
+    if n == '(':
+      if got_brace:
+        tmp += '('
+      got_brace = True
+      unclosed += 1
+      continue
+    if n == ')':
+      unclosed -= 1
+      if got_brace:
+        if unclosed == 0:
+          expanded = calc_2(tmp)
+          final += expanded
+          got_brace = False
+          tmp = ''
+        else:
+          tmp += ')'
+      continue
+    if got_brace:
+      tmp += n
+    else:
+      final += n
+
+  # print('calculating', final)
+
+  # after the braces is gone, we finally can 
+  # evaluate the result using eval function
+  return str(eval(final))
